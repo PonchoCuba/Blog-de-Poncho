d1f12d50e9ea3b75ca7bcf1e43c39b6f9a31282f
Poncho <yosbel1108@gamil.com>
2020-05-09 06:30:19 -0400

    Este es el primer corte del blog

---
+++ aplicaciones/blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from .models import Post, Categoria
+
+def home(request):
+    posts = Post.objects.filter(estado= True)
+    return render(request, 'index.html', {'posts':posts})
+
+def generales(request):
+    posts = Post.objects.filter(
+        estado= True,
+        categoria= Categoria.objects.get(nombre = 'General')
+    )
+    return render(request, 'generales.html', {'posts':posts})
+
+def programacion(request):
+    posts = Post.objects.filter(
+        estado= True,
+        categoria= Categoria.objects.get(nombre = 'Programacion')
+    )
+    return render(request, 'programacion.html', {'posts':posts})
+
+def tecnologia(request):
+    posts = Post.objects.filter(
+        estado= True,
+        categoria= Categoria.objects.get(nombre = 'Tecnologia')
+    )
+    return render(request, 'tecnologia.html', {'posts':posts})
+
+def tutoriales(request):
+    posts = Post.objects.filter(
+        estado= True,
+        categoria= Categoria.objects.get(nombre = 'Tutoriales')
+    )
+    return render(request, 'tutoriales.html', {'posts':posts})
+
+def videojuegos(request):
+    posts = Post.objects.filter(
+        estado= True,
+        categoria= Categoria.objects.get(nombre = 'Videojuegos')
+    )
+    return render(request, 'videojuegos.html', {'posts':posts})
+
